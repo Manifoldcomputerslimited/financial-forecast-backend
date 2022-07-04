d900e285769bed219da35785bf6ac0ad7382d4f8
manasseh liassidji <manassehl9@gmail.com>
2022-07-04 13:58:58 +0100

    bootstrapping

---
+++ server.js
@@ -0,0 +1,21 @@
+//load fastify itself
+const app = require("fastify")({
+  logger: true,
+  trustProxy: true,
+});
+
+// Declare a route
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();

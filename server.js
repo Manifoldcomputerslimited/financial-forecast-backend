fbd31e21f1724eda680eb7b07b16ed5c9d6305f6
manasseh liassidji <manassehl9@gmail.com>
2022-07-31 07:48:35 +0100

    feat: generate zoho access and refresh token

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const db = require("./src/models");
 db.sequelize.sync();
 
 app.register(require("./src/routes/auth.routes.js"), { prefix: "v1" });
+app.register(require("./src/routes/zoho.routes.js"), { prefix: "v1" });
 
 // Declare a route
 app.register(
